0ba1a7a01443bdd6f753561cc803d9f24ac7b014
princehabeeb <habiy3400@gmail.com>
2024-12-08 07:19:00 +0100

    update User.js

--- models/User.js
+++ models/User.js
@@ -1,34 +1,10 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const Joi = require('joi');
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
   email: { type: String, unique: true, required: true },
+  fullName: { type: String, required: true },
   password: { type: String, required: true },
   isVerified: { type: Boolean, default: false },
-  otp: { type: String },
-  otpExpiresAt: { type: Date },
-});
+}, { timestamps: true });
 
-const User = mongoose.model('User', userSchema);
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-
-function validateUser(user) {
-  const schema = Joi.object({
-      fullName: Joi.string().required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
-  });
-  return schema.validate(user);
-}
-
-exports.validate = validateUser;
-exports.User = User;
+module.exports = mongoose.model('User', userSchema);
