d75b9def8d7620e2a4e29e822a46ce84c90c2f3e
princehabeeb <habiy3400@gmail.com>
2024-12-08 07:54:06 +0100

    update 2 files and delete 1 file

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const loadEnvironmentVariables = require("./config/dotenvConfig");
-
-loadEnvironmentVariables();
 
 
 
