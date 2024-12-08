743dc3f7d384eeee675dcda5a4094fbc1dd8b5ab
princehabeeb <habiy3400@gmail.com>
2024-12-08 20:48:45 +0100

    update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 
 const auth = (req, res, next) => {
-    const token = req.header('x-auth-token');
+    const token = req.header('gutenberg-auth-token');
     if (!token) {
         return res.status(401).send('Access denied, no Token provided.');
     }
